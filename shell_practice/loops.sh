d85e092e6574321f5737a0b8072191ad52302206
YadavEshnithin <eshnithinyadav@gmail.com>
2025-08-16 10:00:43 +0530

    update

--- shell_practice/loops.sh
+++ shell_practice/loops.sh
@@ -2,3 +2,4 @@ for i in {1..10000}
 do
     echo $i
 done
+
--- shell_practice/loops_one.sh
+++ shell_practice/loops_one.sh
@@ -5,43 +5,54 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/log/shellscript-logs"
+
+LOGS_FILE="/var/log/shell_scripting_logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-PACKAGES=("mysql" "python" "nginx" "httpd")
+LOGS_FOLDER="$LOGS_FILE/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "nginx" "python")
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+mkdir -p $LOGS_FILE
+echo "we have successfully logs folder created" | tee -a $LOGS_FOLDER
 
-if [ $USERID -ne 0 ]
+if [ $USERID -eq 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
+    echo "You are running with root access" | tee -a $LOGS_FOLDER
 else
-    echo "You are running with root access" | tee -a $LOG_FILE
+    echo "Please run with root access" | tee -a $LOGS_FOLDER
+    exit 1
 fi
 
-# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo "$G...$2 install success $N..." | tee -a $LOGS_FOLDER
     else
-        echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo "$R...$2 install failure $N..." | tee -a $LOGS_FOLDER
         exit 1
     fi
 }
 
-#for package in ${PACKAGES[@]}
-for package in $@
+# dnf list installed mysql 
+
+# if [ $? -eq 0 ]
+# then
+#     echo "mysql already installed...nothing to do" | tee -a $LOGS_FOLDER
+# else
+#     echo "mysql is not installed...installing now" | tee -a $LOGS_FOLDER
+#     dnf install mysql -y
+#     VALIDATE $? "mysql"
+# fi
+
+for i in ${PACKAGES[@]}
 do
-    dnf list installed $package &>>$LOG_FILE
-    if [ $? -ne 0 ]
+    dnf list installed $i 
+
+    if [ $? -eq 0 ]
     then
-        echo "$package is not installed... going to install it" | tee -a $LOG_FILE
-        dnf install $package -y &>>$LOG_FILE
-        VALIDATE $? "$package"
+        echo -e "$Y...$i already installed...nothing to do $N..." | tee -a $LOGS_FOLDER
     else
-        echo -e "Nothing to do $package... $Y already installed $N" | tee -a $LOG_FILE
+        echo "$i  is not installed...installing now" | tee -a $LOGS_FOLDER
+        dnf install $i  -y
+        VALIDATE $? "$i"
     fi
 done
