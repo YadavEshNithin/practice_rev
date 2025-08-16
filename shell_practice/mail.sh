26035fbeca7ea0426a1172d77040f155e8a8ac2e
YadavEshnithin <eshnithinyadav@gmail.com>
2025-08-16 23:12:17 +0530

    update

--- shell_practice/mail.sh
+++ shell_practice/mail.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+# sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "eshnithinyadav@gmail.com" "ALERT-High Disk Usage"
+
 TO_TEAM=$1
 ALERT_TYPE=$2
 IP_ADDRESS=$3
@@ -7,6 +10,9 @@ MSG=$4
 FINAL_MSG=$(printf '%s\n' "$MSG" | sed -e 's/[]\/$*.^[]/\\&/g')
 TO_ADDRESS=$5
 SUBJECT=$6
+
+echo "$MSG"
+echo "$FINAL_MSG"
 
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$FINAL_MSG/g" template.html) 
 
