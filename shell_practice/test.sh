e9846c91b3b81c0a49edf89dc4d628bee6e1ad94
YadavEshnithin <eshnithinyadav@gmail.com>
2025-08-22 17:44:11 +0530

    ansible

---
+++ shell_practice/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define variables
+NAME="John Doe"
+AGE=30
+CITY="New York"
+HEIGHT=1.75
+
+# Example 1: Basic usage with strings and numbers
+# %s for string, %d for integer, \n for newline
+printf "Name: %s\nAge: %d\n" "$NAME" "$AGE"
